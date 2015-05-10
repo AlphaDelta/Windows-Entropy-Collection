a386648c10bfa088d89bcbf1e03c50620f724add
Alfur 'Felt her' Deltur <admin@skid.pro>
2015-05-11 10:30:08 +1200

    Added project

---
+++ WEC-Agent/Program.cs
@@ -0,0 +1,211 @@
+﻿using Gma.UserActivityMonitor;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+namespace Agent
+{
+    static class Program
+    {
+        static volatile bool weregoingdownbuddy = false, itsbeenapleasuretoprocesswithyou = false, tumbling = false;
+
+        [STAThread]
+        static void Main()
+        {
+            TcpListener tcp = null;
+            try
+            {
+                tcp = new TcpListener(IPAddress.Any, 65535);
+                tcp.Start();
+            }
+            catch { MessageBox.Show("The port 65535 cannot be binded to, make sure it is open and another process is not already using it.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error); return; }
+
+            bool free = false;
+            int total = 0, entropyindex = 0, tumble = 0;
+            
+            List<int> Intermediate = new List<int>(256);
+            int[] Proc = new int[0];
+
+            ISAAC isaac = new ISAAC();
+            isaac.Isaac();
+
+            MouseEventHandler dMove = delegate(object sender, MouseEventArgs e)
+            {
+                total += 2;
+
+                Intermediate.Add(e.X);
+                Intermediate.Add(e.Y);
+            };
+            MouseEventHandler dDown = delegate(object sender, MouseEventArgs e)
+            {
+                total++;
+
+                if (e.Button == MouseButtons.Left) Intermediate.Add(0x00);
+                else if (e.Button == MouseButtons.Middle) Intermediate.Add(0x01);
+                else if (e.Button == MouseButtons.Right) Intermediate.Add(0x02);
+                else Intermediate.Add(0x03);
+            };
+            MouseEventHandler dWheel = delegate(object sender, MouseEventArgs e)
+            {
+                total++;
+
+                Intermediate.Add(e.Delta);
+            };
+            KeyEventHandler dKey = delegate(object sender, KeyEventArgs e)
+            {
+                total++;
+
+                Intermediate.Add(e.KeyValue);
+            };
+
+            HookManager.MouseMove += dMove;
+            HookManager.MouseDown += dDown;
+            HookManager.MouseWheel += dWheel;
+            HookManager.KeyDown += dKey;
+
+            NotifyIcon ico = new NotifyIcon();
+            ico.Icon = Properties.Resources.wec;
+
+            ContextMenu cm = new ContextMenu();
+
+            MenuItem cm1 = new MenuItem("Exit...");
+            cm1.DefaultItem = true;
+            cm1.Click += delegate { free = true; };
+            cm.MenuItems.Add(cm1);
+
+            ico.ContextMenu = cm;
+
+            ico.Text = "Windows Entropy Collection";
+            ico.MouseClick += delegate(object sender, MouseEventArgs e)
+            {
+                if(e.Button == MouseButtons.Left)
+                    ico.ShowBalloonTip(3, "Stats", "Entropy collected: " + total + "b\r\nTumbled " + tumble + " times", ToolTipIcon.Info);
+            };
+            ico.Visible = true;
+
+            BackgroundWorker bg = new BackgroundWorker();
+            bg.DoWork += delegate
+            {
+                while (!weregoingdownbuddy)
+                {
+                    total += 3;
+                    Intermediate.Add(Cursor.Position.X);
+                    Intermediate.Add(Cursor.Position.Y);
+                    Intermediate.Add((int)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds);
+
+                    MEMORYSTATUSEX memStatus = new MEMORYSTATUSEX();
+                    if (GlobalMemoryStatusEx(memStatus))
+                    {
+                        total += 3;
+                        Intermediate.Add((int)memStatus.ullAvailPhys);
+                        Intermediate.Add((int)memStatus.ullAvailVirtual);
+                        Intermediate.Add((int)memStatus.ullAvailPageFile);
+                    }
+                    memStatus = null;
+
+                    tumbling = true;
+                    Proc = Intermediate.ToArray();
+                    Intermediate.Clear();
+                    for (int i = 0; i < Proc.Length; i++) isaac.mem[i % ISAAC.SIZE] = isaac.mem[i % ISAAC.SIZE] ^ Proc[i];
+                    isaac.Isaac();
+                    tumbling = false;
+
+                    entropyindex = 0;
+                    tumble++;
+                    for (int i = 0; i < 10; i++) { if (weregoingdownbuddy) break; Thread.Sleep(100); } //Check for exit every 100ms for 10 seconds until the next tumble
+                }
+
+                itsbeenapleasuretoprocesswithyou = true;
+            };
+            bg.RunWorkerAsync();
+
+            BackgroundWorker serve = new BackgroundWorker();
+            serve.DoWork += delegate
+            {
+                while (!weregoingdownbuddy)
+                {
+                    if (tcp.Pending() == true)
+                    {
+                        TcpClient client = tcp.AcceptTcpClient();
+                        NetworkStream st = client.GetStream();
+
+                        st.WriteByte(0x06); //ACK (Acknowledgment)
+
+                        try
+                        {
+                            if (tumble > 2) //Make sure we have tumbled data available and to an acceptable degree, otherwise we would be feeding possibly predictable data
+                            {
+                                int wait = 100;
+                                while (tumbling) { Thread.Sleep(10); }
+                                while (client.Available < 1) { wait--; if (wait < 1) break; Thread.Sleep(10); } //Wait for the client to send us the amount of entropy to release
+
+                                if (wait > 0) //If we havent timed out release the requested amount of entropy, if that amount of entropy isn't available just release whatever we have
+                                {
+                                    int num = (int)st.ReadByte() + entropyindex;
+                                    for (int i = entropyindex; i < num && i < ISAAC.SIZE; i++)
+                                    {
+                                        st.WriteByte((byte)(isaac.rsl[i] % 256));
+                                        entropyindex++;
+                                    }
+                                }
+                            }
+
+                            st.WriteByte(0x04); //EOT (End of Transmission)
+                        }
+                        catch { }
+                        st.Close();
+                    }
+
+                    Thread.Sleep(10);
+                }
+            };
+            serve.RunWorkerAsync();
+
+            while (!free)
+            {
+                Application.DoEvents();
+
+                Thread.Sleep(5);
+            }
+
+            HookManager.MouseMove -= dMove;
+            HookManager.MouseDown -= dDown;
+            HookManager.MouseWheel -= dWheel;
+            HookManager.KeyDown -= dKey;
+
+            weregoingdownbuddy = true;
+            while (!itsbeenapleasuretoprocesswithyou) { Thread.Sleep(100); }
+
+            ico.Dispose();
+            tcp.Stop();
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        private class MEMORYSTATUSEX
+        {
+            public uint dwLength;
+            public uint dwMemoryLoad;
+            public ulong ullTotalPhys;
+            public ulong ullAvailPhys;
+            public ulong ullTotalPageFile;
+            public ulong ullAvailPageFile;
+            public ulong ullTotalVirtual;
+            public ulong ullAvailVirtual;
+            public ulong ullAvailExtendedVirtual;
+            public MEMORYSTATUSEX()
+            {
+                this.dwLength = (uint)Marshal.SizeOf(typeof(MEMORYSTATUSEX));
+            }
+        }
+
+
+        [return: MarshalAs(UnmanagedType.Bool)]
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern bool GlobalMemoryStatusEx([In, Out] MEMORYSTATUSEX lpBuffer);
+    }
+}
---
+++ WEC-Test-Program/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace TestProgram
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.CursorVisible = false;
+
+            int[] table = new int[256];
+            for (int i = 0; i < 256; i++) { table[i] = 0; }
+
+            do
+            {
+                TcpClient tcp = new TcpClient("127.0.0.1", 65535);
+                Console.WriteLine("Listening on localhost:65535");
+
+                while (tcp.Available < 1) Thread.Sleep(100);
+                while (tcp.Available > 0)
+                {
+                    int bt = tcp.GetStream().ReadByte();
+                    if (bt == 0x06)
+                    {
+                        Console.Write("Recieved: ");
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine("ACK");
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Recieved: 0x" + bt.ToString("X2"));
+                        return;
+                    }
+                }
+
+
+                tcp.GetStream().WriteByte(0xFF);
+                Console.Write("Wrote: ");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("0xFF");
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.WriteLine(" and now waiting...");
+                Console.ResetColor();
+
+                while (tcp.Available < 1) Thread.Sleep(100);
+                Console.WriteLine("Reading " + tcp.Available + " bytes   ");
+                int i = 0;
+                while (tcp.Available > 0)
+                {
+                    int b = tcp.GetStream().ReadByte();
+                    if (b == 0x04)
+                    {
+                        //Console.BackgroundColor = ConsoleColor.Red;
+                        //Console.ForegroundColor = ConsoleColor.White;
+                        //Console.Write("XX");
+                        //Console.ResetColor();
+                        //Console.Write(" ");
+                    }
+                    else
+                    {
+                        table[b]++;
+                        //Console.ForegroundColor = ConsoleColor.White;
+                        //Console.Write(b.ToString("X2"));
+                        //Console.Write(" ");
+                    }
+                    //if (i % 8 == 7) Console.WriteLine();
+                    i++;
+                }
+
+                int rnum = 0;
+                for (int num = 0; num < 256; num++)
+                {
+                    int num2 = (table[num] > 0xFF ? 0xFF : table[num]);
+                    //if (num2 < 1) continue;
+
+                    Console.BackgroundColor = (num2 >= 0xFF ? ConsoleColor.Red : ConsoleColor.Black);
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write(num.ToString("X2"));
+                    Console.ForegroundColor = (num2 >= 0xFF ? ConsoleColor.Black : (num2 > 0xBD ? ConsoleColor.Magenta : (num2 >= 0x7E ? ConsoleColor.Red : (num2 >= 0x3F ? ConsoleColor.DarkRed : ConsoleColor.DarkGray))));
+                    Console.Write(" " + (num == 0x04 ? "XX" : num2.ToString("X2")));
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.Write(" ");
+
+                    if (rnum % 8 == 7) Console.WriteLine();
+                    rnum++;
+                }
+
+                tcp.Close();
+                Console.ResetColor();
+                Console.WriteLine("\r\nClosed localhost:65535");
+                Console.SetCursorPosition(0, 0);
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+            Console.ReadKey();
+        }
+    }
+}
